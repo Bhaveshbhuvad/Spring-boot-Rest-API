403febbe938a4ddce600fc0cc1a646102168f3fb
Bhavesh Bhuvad <93036460+Bhaveshbhuvad@users.noreply.github.com>
2022-06-30 12:15:01 +0530

    Update all Api in Controller

--- src/main/java/com/example/demo/controller/myController.java
+++ src/main/java/com/example/demo/controller/myController.java
@@ -3,7 +3,12 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.Course;
@@ -29,4 +34,31 @@ public class myController {
 		System.out.println(courseService.getCourses());
 		return this.courseService.getCourses();
 	}
+	
+	@GetMapping("/courses/{courseId}")
+	public Course getCourse(@PathVariable String courseId)
+	{
+		return this.courseService.getCourse(Long.parseLong(courseId));
+	}
+	
+	@PostMapping("/courses")
+	public Course addCourses(@RequestBody Course course)
+	{
+		return this.courseService.addCourse(course);
+	}
+	
+	 @PutMapping("/courses/{courseId}")
+	 public Course updateCourse(@RequestBody Course course, @PathVariable Long courseId) 
+	 {
+	    return this.courseService.updateCourse(course, courseId);
+	    
+	  }
+	
+	  @DeleteMapping("/courses/{courseId}")
+	  public Course deleteCourse(@PathVariable Long courseId) 
+	  {
+	    return this.courseService.deleteCourse(courseId);
+	  } 
+	
+	
 }
