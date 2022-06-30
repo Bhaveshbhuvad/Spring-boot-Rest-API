60a7ce68c8a707273663971c4729508422d1d8bb
Bhavesh Bhuvad <93036460+Bhaveshbhuvad@users.noreply.github.com>
2022-06-30 12:18:06 +0530

    updated all method of api in serviceImplemented

--- src/main/java/com/example/demo/service/CouseServiceImpl.java
+++ src/main/java/com/example/demo/service/CouseServiceImpl.java
@@ -2,8 +2,10 @@ package com.example.demo.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+
 import com.example.demo.entities.Course;
 
 @Service
@@ -23,5 +25,49 @@ public class CouseServiceImpl implements CourseService{
 		// TODO Auto-generated method stub
 		return list;
 	}
+
+	@Override
+	public Course getCourse(long courseId) {
+
+		Course c=null;
+		for(Course course:list)
+		{
+			if(course.getId()==courseId) 
+			{
+				c=course;
+				break;
+			}
+		}
+		return c;
+	}
+
+	@Override
+	public Course addCourse(Course course) {
+		list.add(course);
+		return course;
+	}
+
+	@Override
+	public Course updateCourse(Course course, Long courseId) {
+		list=list.stream().map(b->{
+			if(b.getId()==courseId)
+			{
+				b.setTitle(course.getTitle());
+				b.setDescrription(course.getDescrription());
+			}
+			return b;
+		}).collect(Collectors.toList());
+		return null;
+	}
+
+	@Override
+	public Course deleteCourse(Long courseId) {
+		list=list.stream().filter(course ->course.getId()!=courseId).collect(Collectors.toList());
+		return null;
+		
+	}
+
+	
+	
 
 }
